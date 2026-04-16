54f8482e8a73f7cb51af061ee058abfdc8c57f09
Hector <hector@saizu.app>
2026-04-16 18:31:25 -0500

    Fix: Añadir importación de useEffect faltante en Panel Editor

--- src/components/EditorPanel.jsx
+++ src/components/EditorPanel.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSuitcase } from '../context/SuitcaseContext';
 import GallerySlots from './GallerySlots';
 import ColorSwatchPicker from './ColorSwatchPicker';
